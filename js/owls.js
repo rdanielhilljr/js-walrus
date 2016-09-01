3559555da096085baab153289a3ca6bf1503fd9d
Danny Hill and Marty Kovach <danny@>
2016-08-31 20:31:13 -0700

    add owl version of website

---
+++ js/owls.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+  $(".p1").click(function() {
+    $(".img1").slideToggle();
+
+  });
+});
+
+$(document).ready(function() {
+  $(".p2").click(function() {
+    $(".img2").fadeToggle();
+
+  });
+});
+
+$(document).ready(function() {
+  $(".p3").click(function() {
+    $(".img3").fadeToggle();
+
+  });
+});
